3b2e973b7dec8f5bcdf5671478f1add9bd399d4c
nsrose <nsrose@berkeley.edu>
2016-08-23 22:06:37 -0700

    about section and start experience section

--- static/js/script.js
+++ static/js/script.js
@@ -19,5 +19,20 @@ $(document).ready(function() {
         $(window).on("scroll load resize", function(){
             checkScroll();
         });
-    }    
+    }
+
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
 })
